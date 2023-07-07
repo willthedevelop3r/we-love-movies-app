a6198c1f056dd0033b451aa151798e5cb08ec503
William Nguyen <williamnguyen773@gmail.com>
2023-07-07 15:51:20 -0500

    readjusted pool

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: DATABASE_URL,
-    pool: { min: 0, max: 10 },
+    pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
@@ -20,7 +20,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: DATABASE_URL,
-    pool: { min: 0, max: 10 },
+    pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
