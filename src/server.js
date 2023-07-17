7bcc4c2273880d1fdc342d559e5bac1b17550417
William Nguyen <williamnguyen773@gmail.com>
2023-07-17 10:23:26 -0500

    removed cron test

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const { PORT = 5001 } = process.env;
 const app = require('./app');
 const knex = require('./db/connection');
 const listener = () => console.log(`Listening on Port ${PORT}!`);
-const cron = require('node-cron');
-const axios = require('axios');
+// const cron = require('node-cron');
+// const axios = require('axios');
 const { BACK_URL } = process.env;
 
 knex.migrate
@@ -14,9 +14,9 @@ knex.migrate
   })
   .catch(console.error);
 
-cron.schedule('*/14 * * * *', () => {
-  axios
-    .get(BACK_URL)
-    .then((response) => console.log(`Status: ${response.status}`))
-    .catch((error) => console.error(error));
-});
+// cron.schedule('*/14 * * * *', () => {
+//   axios
+//     .get(BACK_URL)
+//     .then((response) => console.log(`Status: ${response.status}`))
+//     .catch((error) => console.error(error));
+// });
