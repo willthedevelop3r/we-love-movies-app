6463f25a5f26fe0b325be5c722a37918c3a1fdb4
William Nguyen <williamnguyen773@gmail.com>
2023-07-17 19:48:47 -0500

    uninstalled axios and cron

--- src/server.js
+++ src/server.js
@@ -2,9 +2,6 @@ const { PORT = 5001 } = process.env;
 const app = require('./app');
 const knex = require('./db/connection');
 const listener = () => console.log(`Listening on Port ${PORT}!`);
-// const cron = require('node-cron');
-// const axios = require('axios');
-const { BACK_URL } = process.env;
 
 knex.migrate
   .latest()
@@ -13,10 +10,3 @@ knex.migrate
     app.listen(PORT, listener);
   })
   .catch(console.error);
-
-// cron.schedule('*/14 * * * *', () => {
-//   axios
-//     .get(BACK_URL)
-//     .then((response) => console.log(`Status: ${response.status}`))
-//     .catch((error) => console.error(error));
-// });
